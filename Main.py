9dacd67c0017c6562e06169df47c9659c9e8f26b
Sabaly99 <naniasabaly99@gmail.com>
2026-02-08 16:44:05 +0000

    new commit

--- Main.py
+++ Main.py
@@ -2,3 +2,9 @@
 def dms_to_dd(Degrees,Minutes,Seconds):
     return Degrees + (Minutes/60) + (Seconds/3600)
 
+def DD_to_DMS(DecimalDegrees):
+   Degrees = int(DecimalDegrees)
+   Minutes = int((DecimalDegrees - Degrees) * 60)
+   Seconds = (DecimalDegrees - Degrees - (Minutes/60)) * 3600
+   return Degrees, Minutes, Seconds
+
