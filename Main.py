2350e4525341eca07204e0f27889d07db5d7ca96
Sabaly99 <naniasabaly99@gmail.com>
2026-02-28 00:23:43 +0000

    secoond  function added to the conveter

--- Main.py
+++ Main.py
@@ -34,3 +34,26 @@ def wgs84_to_gng(lat, lon):
         northing_ft = northing * 3.28084
 
         return easting_ft, northing_ft
+
+
+
+
+
+def gng_to_wgs84(easting_ft, northing_ft):
+        """Convert Ghana National Grid (feet) to WGS84 coordinates."""
+        false_easting = 900000
+        false_northing = 0
+        scale_factor = 0.9996
+        central_meridian = -1.0
+    #   Here we divide by the constant 3.28084 to change from feet to meters
+        easting = easting_ft / 3.28084
+        northing = northing_ft / 3.28084
+    # Here convert from GNG to WGS84
+    #The value 6378137 in the code represents the semi-major axis of the WGS84 ellipsoid, measured in meters
+        lon_rad = (easting - false_easting) / (scale_factor * 6378137)
+        lat_rad = (northing - false_northing) / (scale_factor * 6378137)
+   # here we use numpy which was imported as np to convert degrees to radians
+        lat = np.degrees(lat_rad)
+        lon = np.degrees(lon_rad) + central_meridian
+
+        return lat, lon
